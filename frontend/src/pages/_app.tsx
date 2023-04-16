084e5bb89cd38427c9ac6478c0a6bcaac44cdbdf
Kyosuke Fujita <kurofuku01@gmail.com>
2023-04-17 06:25:57 +0900

    frontendをnextJSで作り直した

---
+++ frontend/src/pages/_app.tsx
@@ -0,0 +1,6 @@
+import type { AppProps } from "next/app";
+import "modern-css-reset/dist/reset.min.css";
+
+export default function App({ Component, pageProps }: AppProps) {
+	return <Component {...pageProps} />;
+}
