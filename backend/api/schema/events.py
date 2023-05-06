d93e9243af6aeb888c541c7fefed82e4eb783722
Kyosuke Fujita <kurofuku01@gmail.com>
2023-05-07 08:07:03 +0900

    バックエンドのレスポンスにTimezoneを入れれるようになった

--- backend/api/schema/events.py
+++ backend/api/schema/events.py
@@ -4,5 +4,5 @@ from pydantic import BaseModel
 class Single_Event(BaseModel):
     id: int
     name: str
-    date: str
+    date: str   # timezone付きのdatetime(例:"2023-10-10T23:45:00+0900")
     deleted_at: str | None
